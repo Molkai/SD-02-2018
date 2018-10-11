f8039999227032457f83dbaaccd3652e570b473b
Vinicius Carvalho <vc18051997@gmail.com>
2018-10-11 15:22:01 -0300

    Trabalho T1 sobre Multicast pronto

---
+++ T1/Processos(Teste)/1/Message.java
@@ -0,0 +1,37 @@
+public class Message{
+	private int clock_pid;
+	private int quant;
+	private String text_mes;
+
+	public Message(int c, String text){
+		clock_pid = c;
+		quant = 0;
+		text_mes = text;
+	}
+
+	public Message(int c){
+    clock_pid = c;
+		quant = 1;
+	}
+
+	public void receivedAck(){
+		quant++;
+	}
+
+	public void setMessage(int c, String text){
+		clock_pid = c;
+		text_mes = text;
+	}
+
+	public int getClock(){
+		return clock_pid;
+	}
+
+	public int getQuantAck(){
+		return quant;
+	}
+
+	public String getText(){
+		return text_mes;
+	}
+}
